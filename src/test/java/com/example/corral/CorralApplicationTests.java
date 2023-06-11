9c2776b3d9d8c4114adcc29c329d948cce0033ad
danilsvishchev <svishchevdanil@gmail.com>
2023-06-11 14:38:57 +0300

    init project

---
+++ src/test/java/com/example/corral/CorralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.corral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorralApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
