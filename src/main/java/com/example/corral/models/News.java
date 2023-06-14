b3efc4254dc1f1767448b5e34b12594af82a9519
IlyaSvishchev <pika73559@gmail.com>
2023-06-14 11:50:39 +0300

    it applies to last commit

---
+++ src/main/java/com/example/corral/models/News.java
@@ -0,0 +1,57 @@
+package com.example.corral.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+import org.hibernate.boot.model.relational.Sequence;
+
+@Entity
+public class News {
+    @Id
+    @GeneratedValue(generator = "sequence-generator")
+    @GenericGenerator(
+            name = "sequence-generator",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {
+                    @Parameter(name = "sequence_name", value = "news_id_seq"),
+                    @Parameter(name = "initial_value", value = "1 "),
+                    @Parameter(name = "increment_size", value = "1")}
+    )
+    private Long id;
+
+    private String name;
+    @Column(name = "full_text")
+    private String fullText;
+    public News() {
+    }
+
+    public News(String name, String fullText) {
+        this.name=name;
+        this.fullText = fullText;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+
+
+}
